48c71e857e2ccee19d983565f3f0a1989245ebb6
Rafael Palma <ranpaco@gmail.com>
2019-05-28 15:19:16 -0400

    add new parameters

--- scrapyRealestate/spiders/venta_apartamentos_spider.py
+++ scrapyRealestate/spiders/venta_apartamentos_spider.py
@@ -1,11 +1,11 @@
 import scrapy
 import csv
-
-URL = "https://www.encuentra24.com/panama-es/bienes-raices-venta-de-propiedades-apartamentos.%d"
+BASE_URL = "https://www.encuentra24.com"
+URL = "/panama-es/bienes-raices-venta-de-propiedades-apartamentos.%d"
 
 class QuotesSpider(scrapy.Spider):
     name = "venta_apartamentos"
-    start_urls = URL % 1
+    start_urls = (BASE_URL + URL) % 1
     
     # def __init__(self):
     #     self.page_number = 1  
@@ -24,6 +24,65 @@ class QuotesSpider(scrapy.Spider):
     #     for url in urls:
     #         yield scrapy.Request(url=url, callback=self.parse)
     
+
+    def parse(self, response):
+        print (self.page_number)
+        print ("----------")
+        articles = response.css('article')
+        if not articles:
+            raise CloseSpider('No more pages')
+        for article in articles:
+            #link = article.xpath('//a[@class="more-details"]/@href').extract()
+            link = article.css('a.more-details::attr(href)').get()
+            print(link)
+            absolute_url = BASE_URL + link
+            yield scrapy.Request(url=absolute_url, callback=self.parse_listing)
+            # price = article.css("div.ann-box-details span.ann-price::text").get()
+            # if isinstance(price, str):
+            #     price = price.strip()   
+            # title = article.css("div.ann-box-details a span::text").get()
+            # if title is None:
+            #     title = article.css("div.ann-box-details a strong::text").get()
+            # title = str(title)
+            # if isinstance(title, str):
+            #     title = title.strip()            
+            # desc = article.css("div.ann-box-details span.ann-box-desc::text").get()
+            # if isinstance(desc, str):
+            #     desc = desc.strip()
+            #     desc = desc.replace('\n', ' ')
+            # info = article.css("div.ann-box-details span.ann-info-item::text").extract()[-1]
+            # if isinstance(info, str):
+            #     info = info.strip()
+            # companyName = article.css("div.ann-box-contact span.company-name::text").extract()
+            # if isinstance(companyName, list):
+            #     companyName = ' '.join(companyName)
+            # if isinstance(companyName, str):
+            #     companyName = companyName.replace('\n', ' ')
+            #     companyName = companyName.strip()
+            # phone = article.css("div.ann-box-contact span.phone::text").extract()
+            # print(phone)
+            # if not phone:
+            #     phone = ''
+            # else:
+            #     if isinstance(phone, list):
+            #         if len(phone) > 1:
+            #             phone = phone[1]
+            #         else:
+            #             phone = phone[0]
+            #     if isinstance(phone, str):
+            #         phone = phone.strip()
+            # filename = 'venta-de-apartamentos.csv'
+            # row = [title, desc, price, info, companyName, phone]
+            # with open(filename, mode='a', encoding='utf-8') as csvFile:
+            #     writer = csv.writer(csvFile)
+            #     writer.writerow(row)
+            # csvFile.close()
+
+            #yield 'Saved file %s' % filename
+        self.page_number += 1
+        yield scrapy.Request(url=(BASE_URL + URL) % self.page_number, callback=self.parse)
+
+    def parse_listing(self, response):
     #info = response.css('div.ad-info')
     #categoria: info.xpath('//span[contains(@class, "info-name") and contains(text(), "Categoria:")]/following-sibling::span/text()').get()
     #localizacion: info.xpath('//span[contains(@class, "info-name") and contains(text(), "Localización:")]/following-sibling::span/text()').get()
@@ -35,55 +94,21 @@ class QuotesSpider(scrapy.Spider):
 
     #propietario: response.xpath('//div[contains(@class,"user-info")]/a/span[contains(@class,"user-name")]/text()').get()
     #telefono: response.xpath('//div[contains(@class,"contact-phone")]/span[contains(@class,"phone")]/text()').get()
-    def parse(self, response):
-        print (self.page_number)
-        print ("----------")
-        articles = response.css('article')
-        if not articles:
-            raise CloseSpider('No more pages')
-        for article in articles:    
-            price = article.css("div.ann-box-details span.ann-price::text").get()
-            if isinstance(price, str):
-                price = price.strip()
-            title = article.css("div.ann-box-details a span::text").get()
-            if title is None:
-                title = article.css("div.ann-box-details a strong::text").get()
-            title = str(title)
-            if isinstance(title, str):
-                title = title.strip()            
-            desc = article.css("div.ann-box-details span.ann-box-desc::text").get()
-            if isinstance(desc, str):
-                desc = desc.strip()
-                desc = desc.replace('\n', ' ')
-            info = article.css("div.ann-box-details span.ann-info-item::text").extract()[-1]
-            if isinstance(info, str):
-                info = info.strip()
-            companyName = article.css("div.ann-box-contact span.company-name::text").extract()
-            if isinstance(companyName, list):
-                companyName = ' '.join(companyName)
-            if isinstance(companyName, str):
-                companyName = companyName.replace('\n', ' ')
-                companyName = companyName.strip()
-            phone = article.css("div.ann-box-contact span.phone::text").extract()
-            print(phone)
-            if not phone:
-                phone = ''
-            else:
-                if isinstance(phone, list):
-                    if len(phone) > 1:
-                        phone = phone[1]
-                    else:
-                        phone = phone[0]
-                if isinstance(phone, str):
-                    phone = phone.strip()
-            #page = response.url.split(".")[-2]
-            filename = 'venta-de-apartamentos.csv'
-            row = [title, desc, price, info, companyName, phone]
-            #row = [title]
-            with open(filename, mode='a', encoding='utf-8') as csvFile:
-                writer = csv.writer(csvFile)
-                writer.writerow(row)
-            csvFile.close()
-            #yield 'Saved file %s' % filename
-        self.page_number += 1
-        yield scrapy.Request(url=URL % self.page_number, callback=self.parse)
+        info = response.css('div.ad-info')
+        categoria = info.xpath('//span[contains(@class, "info-name") and contains(text(), "Categoria:")]/following-sibling::span/text()').get()
+        localizacion = info.xpath('//span[contains(@class, "info-name") and contains(text(), "Localización:")]/following-sibling::span/text()').get()
+        precio = info.xpath('//span[contains(@class, "info-name") and contains(text(), "Precio:")]/following-sibling::span/text()').get()
+        details = response.css('div.ad-details')
+        recamaras = details.xpath('//span[contains(@class, "info-name") and contains(text(), "Recámaras:")]/following-sibling::span/text()').get()
+        banos = details.xpath('//span[contains(@class, "info-name") and contains(text(), "Baños:")]/following-sibling::span/text()').get()
+        metros = details.xpath('//span[contains(@class, "info-name") and re:test(text(),"^M² de construcción:$")]/following-sibling::span/text()').get()
+
+        propietario= response.xpath('//div[contains(@class,"user-info")]/a/span[contains(@class,"user-name")]/text()').get()
+        telefono= response.xpath('//div[contains(@class,"contact-phone")]/span[contains(@class,"phone")]/text()').get()        
+        filename = 'venta-de-apartamentos.csv'
+        row = [propietario, telefono, categoria, localizacion, precio, metros, recamaras, banos, response.url]
+        print(row)
+        with open(filename, mode='a', encoding='utf-8') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()        
